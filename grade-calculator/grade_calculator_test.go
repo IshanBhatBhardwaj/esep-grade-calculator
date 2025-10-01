50abc32acc3d7436de4a3b279dc623b4a634928a
IshanBhatBhardwaj <ishanbhatbhardwaj@icloud.com>
2025-10-01 15:03:59 -0400

    added more tests for 100% coverage

--- grade-calculator/grade_calculator_test.go
+++ grade-calculator/grade_calculator_test.go
@@ -1,5 +1,5 @@
 package esepunittests
-
+import "fmt"
 import "testing"
 
 func TestGetGradeA(t *testing.T) {
@@ -10,6 +10,7 @@ func TestGetGradeA(t *testing.T) {
 	gradeCalculator.AddGrade("open source assignment", 100, Assignment)
 	gradeCalculator.AddGrade("exam 1", 100, Exam)
 	gradeCalculator.AddGrade("essay on ai ethics", 100, Essay)
+
 
 	actual_value := gradeCalculator.GetFinalGrade()
 
@@ -26,6 +27,40 @@ func TestGetGradeB(t *testing.T) {
 	gradeCalculator.AddGrade("open source assignment", 80, Assignment)
 	gradeCalculator.AddGrade("exam 1", 81, Exam)
 	gradeCalculator.AddGrade("essay on ai ethics", 85, Essay)
+
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+func TestGetGradeC(t *testing.T) {
+	expected_value := "C"
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("open source assignment", 70, Assignment)
+	gradeCalculator.AddGrade("exam 1", 75, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 73, Essay)
+
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+func TestGetGradeD(t *testing.T) {
+	expected_value := "D"
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("open source assignment", 62, Assignment)
+	gradeCalculator.AddGrade("exam 1", 67, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 69, Essay)
 
 	actual_value := gradeCalculator.GetFinalGrade()
 
@@ -49,3 +84,11 @@ func TestGetGradeF(t *testing.T) {
 		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
 	}
 }
+
+
+func TestPrinting(t *testing.T) {
+	fmt.Print(Assignment)
+	fmt.Print(Essay)
+	fmt.Print(Exam)
+}
+
