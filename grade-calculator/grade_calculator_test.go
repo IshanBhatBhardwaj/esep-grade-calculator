cd32e569b3be3d44499a0f004001ebd9be88e209
IshanBhatBhardwaj <ishanbhatbhardwaj@icloud.com>
2025-10-02 08:42:46 -0400

    added new tests to test pass or fail

--- grade-calculator/grade_calculator_test.go
+++ grade-calculator/grade_calculator_test.go
@@ -92,3 +92,38 @@ func TestPrinting(t *testing.T) {
 	fmt.Print(Exam)
 }
 
+func TestPass(t *testing.T) {
+	expected_value = "Pass"
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("open source assignment", 100, Assignment)
+	gradeCalculator.AddGrade("exam 1", 100, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 100, Essay)
+
+
+	actual_value := gradeCalculator.GetPassOrFail()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+
+}
+
+func TestFail(t *testing.T) {
+	expected_value = "Fail"
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("open source assignment", 40, Assignment)
+	gradeCalculator.AddGrade("exam 1", 40, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 40, Essay)
+
+
+	actual_value := gradeCalculator.GetPassOrFail()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+
+}
